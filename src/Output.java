29b9833900042c214de0aec324bedf3451ce3ef8
Vadim Balanciuc <b.vadik2017@gmail.com>
2023-03-29 01:10:36 +0200

    add Output methods idea

--- src/Output.java
+++ src/Output.java
@@ -1,2 +1,30 @@
+import java.io.BufferedReader;
+import java.util.List;
+
 public class Output {
+
+  public static void showTasksByCategory(BufferedReader br, List<Task> taskList) {
+//todo вывод задач по категориям
+  }
+
+  public static void showTasksByPriority(BufferedReader br, List<Task> taskList) {
+//todo вывод задач по приоритету
+  }
+
+  public static void showTasksByIsDone(BufferedReader br, List<Task> taskList) {
+//todo вывод задач по статусу
+  }
+
+  public static void showAllTasks(BufferedReader br, List<Task> taskList) {
+//todo вывод всех задач в таблицу
+  }
+
+  public static void showTaskById(List<Task> taskList, int id) {
+//todo вывод задачи по Id, детально
+  }
+
+  public static void writeTaskToFile(List<Task> taskList) {
+    //todo сохранение всех задач в файл
+  }
+
 }
