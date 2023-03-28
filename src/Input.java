f5ab98a4c394936400e885c35f3a8feac3491818
Vadim Balanciuc <b.vadik2017@gmail.com>
2023-03-29 01:05:00 +0200

    add Input methods idea

--- src/Input.java
+++ src/Input.java
@@ -1,2 +1,37 @@
+import java.io.*;
+import java.util.List;
+
 public class Input {
+
+  public static int readIntLimited(int min, int max) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int num = 0;
+    do {
+      try {
+        num = Integer.parseInt(br.readLine());
+      } catch (NumberFormatException e) {
+        System.out.println("Вводите только цифры!");
+      }
+      if (!(num >= min && num <= max)) {
+        System.out.printf("Введите число от %d до %d: ", min, max);
+      }
+    } while (!(num >= min && num <= max));
+    return num;
+  }
+
+  public static void readTaskFromFile(List<Task> taskList) {
+    //todo метод для парсинга файла
+  }
+
+  public static Priority takePriority() {
+    Priority priority = null;
+    //todo метод для получение приоритета задачи
+    return priority;
+  }
+
+  public static Category takeCategory() {
+    Category category = null;
+    //todo метод для получение категории задачи
+    return category;
+  }
 }
