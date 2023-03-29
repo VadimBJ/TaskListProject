f095224be3edf2b327d9094260bd52a6b6aa812e
Vadim Balanciuc <b.vadik2017@gmail.com>
2023-03-29 18:08:15 +0200

    add Task code

--- src/Task.java
+++ src/Task.java
@@ -1,71 +1,84 @@
-import java.util.Date;
-
 public class Task {
-    private static int count = 0;
-    private int id; //уникальный идентификатор задачи
-    private String title;//название задачи
-    private String description; // описание задачи
-    private Category category; // enum категории
-    private Priority priority; // enum приоритет
-    private Date planDate; // дата, на которую запланирована задача
+  private static int count = 0;
+  private int id; //уникальный идентификатор задачи
+  private String title;//название задачи
+  private String description; // описание задачи
+  private Category category; // enum категории
+  private Priority priority; // enum приоритет
+  private String date; // дата, на которую запланирована задача
+  private boolean isDone; // флаг выполнения
 
-    private Date createDate;
-    private boolean status; // флаг выполнения
+  public Task(String title, String description, Category category, Priority priority,
+              String date, boolean isDone) {
+    this.id = ++count;
+    this.title = title;
+    this.description = description;
+    this.category = category;
+    this.priority = priority;
+    this.date = date;
+    this.isDone = isDone;
+  }
 
-    public Task(String title, String description, Category category, Priority priority,
-                Date planDate, boolean status) {
-        this.id = ++count;
-        this.title = title;
-        this.description = description;
-        this.category = category;
-        this.priority = priority;
-        this.planDate = planDate;
-        this.status = status;
+  @Override
+  public String toString() {
+    String id1 = "00" + id;
+    id1 = id1.substring(id1.length() - 2);
+    String title1 = title;
+    if (title1.length() > 24) {
+      title1 = title1.substring(0, 22) + "..";
     }
-
-    public static int getCount() {
-        return count;
+    String description1 = description;
+    if (description1.isEmpty()) {
+      description1 = " -не указано-";
+    } else if (description1.length() > 39) {
+      description1 = description1.substring(0, 37) + "..";
     }
-
-    public int getId() {
-        return id;
+    String category1 = category.getName();
+    String priority1 = priority.getName();
+    String date1 = date;
+    if (date1.isEmpty()) {
+      date1 = "не указано";
     }
+    String isDone1 = isDone ? "✔️" : "⏰";
+    return String.format("""
+            │%3s │%-25s│%-40s│%10s │   %10s │%11s│%s  %s️   │%n""",
+        id1, title1, description1, category1, priority1, date1, Menu.SPACE, isDone1);
+  }
 
-    public String getTitle() {
-        return title;
-    }
+  public int getId() {
+    return id;
+  }
 
-    public String getDescription() {
-        return description;
-    }
+  public String getTitle() {
+    return title;
+  }
 
-    public Category getCategory() {
-        return category;
-    }
+  public String getDescription() {
+    return description;
+  }
 
-    public Priority getPriority() {
-        return priority;
-    }
+  public Category getCategory() {
+    return category;
+  }
 
-    public Date getPlanDate() {
-        return planDate;
-    }
+  public Priority getPriority() {
+    return priority;
+  }
 
-    public void setStatus(boolean status) {
-        this.status = status;
-    }
+  public String getDate() {
+    return date;
+  }
 
-    public boolean isStatus() {
-        return status;
-    }
+  public boolean getIsDone() {
+    return isDone;
+  }
 
-    public Date getCreateDate() {
-        return createDate;
-    }
+  public void markAsDone() {
+    this.isDone = true;
+  }
 
-    public void setCreateDate(Date createDate) {
-        this.createDate = createDate;
-    }
+  public void markAsUndone() {
+    this.isDone = false;
+  }
+
 }
-
-
