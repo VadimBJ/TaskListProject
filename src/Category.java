a32f36109508636880a8d0f1c607d270de2ec06c
Vadim Balanciuc <b.vadik2017@gmail.com>
2023-03-29 18:05:56 +0200

    add Category code

--- src/Category.java
+++ src/Category.java
@@ -1,8 +1,24 @@
 public enum Category {
-  WORK,
-  PERSONAL,
-  SHOPPING,
-  HOME,
-  OTHER,
+  WORK(1, "Работа"),
+  PERSONAL(2, "Личные"),
+  SHOPPING(3, "Закупки"),
+  HOME(4, "Дом"),
+  OTHER(5, "Другие");
+  private final int num;
+  private final String categoryName;
+
+
+  Category(int num, String categoryName) {
+    this.num = num;
+    this.categoryName = categoryName;
+  }
+
+  public int getNum() {
+    return num;
+  }
+
+  public String getName() {
+    return categoryName;
+  }
 
 }
