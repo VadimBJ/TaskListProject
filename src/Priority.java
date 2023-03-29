ff6cc9380f7274ac97be13f726e2c49316cf0182
Vadim Balanciuc <b.vadik2017@gmail.com>
2023-03-29 18:06:19 +0200

    add Priority code

--- src/Priority.java
+++ src/Priority.java
@@ -1,5 +1,21 @@
 public enum Priority {
-  HIGH,
-  MEDIUM,
-  LOW,
+  HIGH(1,"\u001B[31mВысокий\u001B[0m"),
+  MEDIUM(2,"\u001B[33mСредний\u001B[0m"),
+  LOW(3,"\u001B[32mНизкий \u001B[0m");
+
+  private final int num;
+  private final String name;
+
+  Priority(int num, String name) {
+    this.num = num;
+    this.name = name;
+  }
+
+  public int getNum() {
+    return num;
+  }
+
+  public String getName() {
+    return name;
+  }
 }
