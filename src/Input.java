c2e06888a42160747ed0b32ca231a33d45fd19c6
Vadim Balanciuc <b.vadik2017@gmail.com>
2023-03-29 18:07:00 +0200

    add Input code

--- src/Input.java
+++ src/Input.java
@@ -1,7 +1,36 @@
 import java.io.*;
 import java.util.List;
 
-public class Input {
+public class Input implements Finals {
+  private static boolean isRead = false;
+
+  public static void readTaskFromFile(List<Task> taskList) throws IOException {
+    if (!isRead) {
+      File file = new File("res/TaskList.csv"); //todo добавить проверку есть ли файл
+      BufferedReader fr = new BufferedReader(new FileReader(file));
+
+      for (String line = fr.readLine(); line != null; line = fr.readLine()) {
+        String[] lines = line.split(";",-1);
+        String title = lines[0];
+        String description = lines[1];
+        if (description.isEmpty()) {
+          description = " -не указано-";
+        }
+        Category category = Category.valueOf(lines[2]);
+        Priority priority = Priority.valueOf(lines[3]);
+        String date = lines[4]; //todo добавить работу с типом Data
+        if (date.isEmpty()) {
+          date = "не указано";
+        }
+        boolean isDone = lines[5].equals("1");
+        Task tmpTask = new Task(title, description, category, priority, date, isDone);
+        taskList.add(tmpTask);
+      }
+      fr.close();
+      isRead = true;
+    }
+    System.out.println(CYAN + "... Файл прочитан ..." + RESET);
+  }
 
   public static int readIntLimited(int min, int max) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -19,19 +48,39 @@ public class Input {
     return num;
   }
 
-  public static void readTaskFromFile(List<Task> taskList) {
-    //todo метод для парсинга файла
-  }
-
-  public static Priority takePriority() {
+  public static Priority takePriority() throws IOException {
+    int lastofnum = 1;
+    System.out.println("Выберите приоритет задачи:");
+    for (Priority values : Priority.values()) {
+      System.out.println("  " + values.getNum() + ". " + values.getName());
+      lastofnum = values.getNum();
+    }
+    System.out.print("Введите номер пункта меню: ");
+    int choice = readIntLimited(1, lastofnum);
     Priority priority = null;
-    //todo метод для получение приоритета задачи
+    for (Priority values : Priority.values()) {
+      if (values.getNum() == choice) {
+        priority = values;
+      }
+    }
     return priority;
   }
 
-  public static Category takeCategory() {
+  public static Category takeCategory() throws IOException {
+    int lastofnum = 1;
+    System.out.println("Выберите категорию задачи:");
+    for (Category values : Category.values()) {
+      System.out.println("  " + values.getNum() + ". " + values.getName());
+      lastofnum = values.getNum();
+    }
+    System.out.print("Введите номер пункта меню: ");
+    int choice = readIntLimited(1, lastofnum);
     Category category = null;
-    //todo метод для получение категории задачи
+    for (Category values : Category.values()) {
+      if (values.getNum() == choice) {
+        category = values;
+      }
+    }
     return category;
   }
 }
