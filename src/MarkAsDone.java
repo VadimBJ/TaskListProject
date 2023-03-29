8a1217e6d2ad92b4063c845a5b506153ce68408b
Korney Saushkin <saukorn03@gmail.com>
2023-03-29 21:14:57 +0200

    Add class MarkAsDone

---
+++ src/MarkAsDone.java
@@ -0,0 +1,11 @@
+import java.io.BufferedReader;
+
+public class MarkAsDone {
+  public static void markAsDone(BufferedReader br, Task task){
+    if(task.getIsDone()==false){
+      task.markAsDone();
+    } else{
+      System.out.println("Оно уже выполнено!");
+    }
+  }
+}
