07f60f2fc3831f00dda7aa653aca667ba5907b06
Vadim Balanciuc <b.vadik2017@gmail.com>
2023-03-30 22:15:04 +0200

    refactor SortCategoryAndName

--- src/SortCategoryAndName.java
+++ src/SortCategoryAndName.java
@@ -3,11 +3,9 @@ import java.util.Comparator;
 public class SortCategoryAndName implements Comparator<Task> {
   @Override
   public int compare(Task o1, Task o2) {
-    if (o1.getCategory().getName().compareTo(o2.getCategory().getName())==0) {
-      o1.getTitle().compareTo(o2.getTitle());
+    if (o1.getCategory().getName().compareTo(o2.getCategory().getName()) == 0) {
+      return o1.getTitle().compareTo(o2.getTitle());
     }
     return o1.getCategory().getName().compareTo(o2.getCategory().getName());
   }
-
 }
-
