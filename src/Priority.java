79bea959c48726121d433666eee28d4010332ba8
Vadim Balanciuc <b.vadik2017@gmail.com>
2023-03-30 22:14:35 +0200

    refactor Priority

--- src/Priority.java
+++ src/Priority.java
@@ -1,7 +1,7 @@
 public enum Priority {
-  HIGH(1,"\u001B[31mВысокий\u001B[0m"),
-  MEDIUM(2,"\u001B[33mСредний\u001B[0m"),
-  LOW(3,"\u001B[32mНизкий \u001B[0m");
+  HIGH(1, "\u001B[31mВысокий\u001B[0m"),
+  MEDIUM(2, "\u001B[33mСредний\u001B[0m"),
+  LOW(3, "\u001B[32mНизкий \u001B[0m");
 
   private final int num;
   private final String name;
