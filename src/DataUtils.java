f77dd65bf85eededca6f07a932bb2c10850cc8c2
Vadim Balanciuc <b.vadik2017@gmail.com>
2023-03-30 22:13:35 +0200

    refactor DataUtils

--- src/DataUtils.java
+++ src/DataUtils.java
@@ -5,62 +5,61 @@ import java.util.Date;
 import java.util.GregorianCalendar;
 
 public class DataUtils {
-    public static String getDateToStr(Date date) {
-        String dateStr;
-        if (date != null) {
-            SimpleDateFormat format = new SimpleDateFormat("yyyy.MM.dd");
-            return dateStr = format.format(date);
-        }
-        return dateStr = "не указано";
+  public static String getDateToStr(Date date) {
+    if (date != null) {
+      SimpleDateFormat format = new SimpleDateFormat("yyyy.MM.dd");
+      return format.format(date);
     }
+    return "не указано";
+  }
 
-    public static String getDateToStrToFile(Date date) {
-        String dateStr = "";
-        if (date != null) {
-            SimpleDateFormat format = new SimpleDateFormat("yyyy.MM.dd");
-            return dateStr = format.format(date);
-        }
-        return dateStr;
+  public static String getDateToStrToFile(Date date) {
+    String dateStr = "";
+    if (date != null) {
+      SimpleDateFormat format = new SimpleDateFormat("yyyy.MM.dd");
+      return format.format(date);
     }
+    return dateStr;
+  }
 
-    public static Date getDataFromStr(String dateStr) throws ParseException {
-        if (dateStr.isEmpty()) {
-            return null;
-        }
-        SimpleDateFormat format = new SimpleDateFormat("yyyy.MM.dd");
-        Date date = format.parse(dateStr);
-        return date;
+  public static Date getDataFromStr(String dateStr) throws ParseException {
+    if (dateStr.isEmpty()) {
+      return null;
     }
+    SimpleDateFormat format = new SimpleDateFormat("yyyy.MM.dd");
+    Date date = format.parse(dateStr);
+    return date;
+  }
 
-    public static boolean checkValidDate(Date date) {
-        boolean result = false;
-        Date dataNow = dateRemoveTime(new Date());
-        if (date.compareTo(dataNow) == 0 || date.after(dataNow)) {
-            result = true;
-        }
-        return result;
+  public static boolean checkValidDate(Date date) {
+    boolean result = false;
+    Date dataNow = dateRemoveTime(new Date());
+    if (date.compareTo(dataNow) == 0 || date.after(dataNow)) {
+      result = true;
     }
+    return result;
+  }
 
-    private static Date dateRemoveTime(Date date) {
-        Calendar calendar = new GregorianCalendar();
-        calendar.setTime(date);
-        calendar.set(Calendar.HOUR_OF_DAY, 0);
-        calendar.set(Calendar.MINUTE, 0);
-        calendar.set(Calendar.SECOND, 0);
-        calendar.set(Calendar.MILLISECOND, 0);
+  private static Date dateRemoveTime(Date date) {
+    Calendar calendar = new GregorianCalendar();
+    calendar.setTime(date);
+    calendar.set(Calendar.HOUR_OF_DAY, 0);
+    calendar.set(Calendar.MINUTE, 0);
+    calendar.set(Calendar.SECOND, 0);
+    calendar.set(Calendar.MILLISECOND, 0);
 
-        return calendar.getTime();
+    return calendar.getTime();
+  }
+
+  public static boolean checkFormatData(String dateStr) {
+    boolean result = false;
+    int month = Integer.parseInt(dateStr.substring(5, 7));
+    int day = Integer.parseInt(dateStr.substring(8, 10));
+    if (month >= 1 && month <= 12) {
+      if (day >= 1 && day <= 31) {
+        return true;
+      }
     }
-
-    public static boolean checkFormatData(String dateStr) {
-        boolean result = false;
-        int month = Integer.parseInt(dateStr.substring(5, 7));
-        int day = Integer.parseInt(dateStr.substring(8, 10));
-        if (month >= 1 && month <= 12) {
-            if (day >= 1 && day <= 31) {
-                return true;
-            }
-        }
-        return result;
-    }
+    return result;
+  }
 }
