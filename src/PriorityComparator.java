7d0ef82fb2c123884674ffe75da569c7ca9b3903
Korney Saushkin <saukorn03@gmail.com>
2023-03-30 21:17:09 +0200

    Delete useless classes PriorityComparator and StatusComparator

--- src/PriorityComparator.java
+++
@@ -1,8 +0,0 @@
-import java.util.Comparator;
-
-public class PriorityComparator implements Comparator<Task> {
-  @Override
-  public int compare(Task o1, Task o2) {
-    return o1.getPriority().getNum() - o2.getPriority().getNum();
-  }
-}
