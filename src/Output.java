c715d0988dcd3bc50bc20ce04a79206e7042b3d7
Vadim Balanciuc <b.vadik2017@gmail.com>
2023-03-30 19:46:35 +0200

    add taskDeleteById

--- src/Output.java
+++ src/Output.java
@@ -6,144 +6,143 @@ import java.util.Collections;
 import java.util.List;
 
 public class Output implements Finals {
-    public static String headerColorSort = String.format(HEADER, RESET, RESET, RESET, RESET, RESET, RESET,
-            RESET, RESET);
-    public static String headerColorFilter = String.format(HEADER, RESET, RESET, RESET, RESET, RESET,
-            RESET,
-            RESET, RESET);
+  public static String headerColorSort = String.format(HEADER, RESET, RESET, RESET, RESET, RESET, RESET,
+      RESET, RESET);
+  public static String headerColorFilter = String.format(HEADER, RESET, RESET, RESET, RESET, RESET,
+      RESET,
+      RESET, RESET);
 
-    public static void showTasksByCategory(BufferedReader br, List<Task> taskList) throws IOException {
-        Category category = Input.takeCategory();
-        System.out.println();
-        headerColorFilter = String.format(HEADER, PURPLE, RESET, RESET, RESET, RESET, RESET, RESET, RESET);
-        System.out.println(headerColorFilter);
-        for (Task task : taskList) {
-            if (task.getCategory().equals(category)) {
-                System.out.print(task);
-            }
-        }
-        System.out.println(FOOTER);
-        Menu.menuTaskListShow(br, taskList);
+  public static void showTasksByCategory(BufferedReader br, List<Task> taskList) throws IOException {
+    Category category = Input.takeCategory();
+    System.out.println();
+    headerColorFilter = String.format(HEADER, PURPLE, RESET, RESET, RESET, RESET, RESET, RESET, RESET);
+    System.out.println(headerColorFilter);
+    for (Task task : taskList) {
+      if (task.getCategory().equals(category)) {
+        System.out.print(task);
+      }
     }
+    System.out.println(FOOTER);
+    Menu.menuTaskListShow(br, taskList);
+  }
 
-    public static void showTasksByPriority(BufferedReader br, List<Task> taskList) throws IOException {
-        Priority priority = Input.takePriority();
-        System.out.println();
-        headerColorFilter = String.format(HEADER, RESET, RESET, PURPLE, RESET, RESET, RESET, RESET, RESET);
-        System.out.println(headerColorFilter);
-        for (Task task : taskList) {
-            if (task.getPriority().equals(priority)) {
-                System.out.print(task);
-            }
-        }
-        System.out.println(FOOTER);
-        Menu.menuTaskListShow(br, taskList);
+  public static void showTasksByPriority(BufferedReader br, List<Task> taskList) throws IOException {
+    Priority priority = Input.takePriority();
+    System.out.println();
+    headerColorFilter = String.format(HEADER, RESET, RESET, PURPLE, RESET, RESET, RESET, RESET, RESET);
+    System.out.println(headerColorFilter);
+    for (Task task : taskList) {
+      if (task.getPriority().equals(priority)) {
+        System.out.print(task);
+      }
     }
+    System.out.println(FOOTER);
+    Menu.menuTaskListShow(br, taskList);
+  }
 
-    public static void showTasksByIsDone(BufferedReader br, List<Task> taskList, boolean flag) throws IOException {
-        System.out.println();
-        headerColorFilter = String.format(HEADER, RESET, RESET, RESET, RESET, RESET, RESET, PURPLE, RESET);
-        System.out.println(headerColorFilter);
-        for (Task task : taskList) {
-            if (task.getIsDone() == flag) {
-                System.out.print(task);
-            }
-        }
-        System.out.println(FOOTER);
-        Menu.menuTaskListShow(br, taskList);
+  public static void showTasksByIsDone(BufferedReader br, List<Task> taskList, boolean flag) throws IOException {
+    System.out.println();
+    headerColorFilter = String.format(HEADER, RESET, RESET, RESET, RESET, RESET, RESET, PURPLE, RESET);
+    System.out.println(headerColorFilter);
+    for (Task task : taskList) {
+      if (task.getIsDone() == flag) {
+        System.out.print(task);
+      }
     }
+    System.out.println(FOOTER);
+    Menu.menuTaskListShow(br, taskList);
+  }
 
-    public static void showAllTasks(BufferedReader br, List<Task> taskList) throws IOException {
-        System.out.println();
-        System.out.println(headerColorSort);
-        for (Task task : taskList) {
-            System.out.print(task);
-        }
-        System.out.println(FOOTER);
-        Menu.menuTaskListShow(br, taskList);
-        showAllTasks(br, taskList);
+  public static void showAllTasks(BufferedReader br, List<Task> taskList) throws IOException {
+    System.out.println();
+    System.out.println(headerColorSort);
+    for (Task task : taskList) {
+      System.out.print(task);
     }
+    System.out.println(FOOTER);
+    Menu.menuTaskListShow(br, taskList);
+    showAllTasks(br, taskList);
+  }
 
-    public static void showTaskById(List<Task> taskList, int id) {
-        Task task = taskList.get(id);
-        System.out.println("─".repeat(95));
-        System.out.println(BLUE + "Задача " + task.getId() + ": " + RESET + task.getTitle());
-        System.out.println();
-        if (!task.getDescription().trim().isEmpty() && !task.getDescription().contains("-не указано-")) {
-            String[] description = task.getDescription().split(" ");
-            int symbolCount = 0;
-            for (String s : description) {
-                symbolCount += s.length();
-                System.out.print(s + " ");
-                if (symbolCount > 70) {
-                    System.out.println();
-                    symbolCount = 0;
-                }
-            }
+  public static void showTaskById(List<Task> taskList, int id) {
+    Task task = taskList.get(id); //todo сделать поиск id перебором
+    System.out.println("─".repeat(95));
+    System.out.println(BLUE + "Задача " + task.getId() + ": " + RESET + task.getTitle());
+    System.out.println();
+    if (!task.getDescription().trim().isEmpty() && !task.getDescription().contains("-не указано-")) {
+      String[] description = task.getDescription().split(" ");
+      int symbolCount = 0;
+      for (String s : description) {
+        symbolCount += s.length();
+        System.out.print(s + " ");
+        if (symbolCount > 70) {
+          System.out.println();
+          symbolCount = 0;
         }
-        System.out.println("\nДата: " + DataUtils.getDateToStr(task.getPlanDate()));
-        System.out.println();
-        System.out.println(BLUE + "Категория: " + RESET + task.getCategory().getName());
-        System.out.println(BLUE + "Приоритет: " + task.getPriority().getName());
-        System.out.println(BLUE + "Статус задачи: " + RESET + (task.getIsDone() ? "Выполнено" : "Не выполнено"));
-        System.out.println("─".repeat(60));
-        System.out.println();
+      }
     }
+    System.out.println("\nДата: " + DataUtils.getDateToStr(task.getPlanDate()));
+    System.out.println();
+    System.out.println(BLUE + "Категория: " + RESET + task.getCategory().getName());
+    System.out.println(BLUE + "Приоритет: " + task.getPriority().getName());
+    System.out.println(BLUE + "Статус задачи: " + RESET + (task.getIsDone() ? "Выполнено" : "Не выполнено"));
+    System.out.println("─".repeat(60));
+    System.out.println();
+  }
 
-    public static void sortByStatusAndDataPlan(BufferedReader br, List<Task> taskList) throws IOException {
-        System.out.println();
-        headerColorSort = String.format(HEADER, RESET, RESET, RESET, RESET, RESET, RESET, PURPLE, RESET);
-        System.out.println(headerColorSort);
-        SortStatusAndDatePlan sortRule = new SortStatusAndDatePlan();
-        Collections.sort(taskList, sortRule);
-        for (Task task : taskList) {
-            System.out.print(task);
-        }
-        System.out.println(FOOTER);
-        Menu.menuSortChoice(br, taskList);
+  public static void sortByStatusAndDataPlan(BufferedReader br, List<Task> taskList) throws IOException {
+    System.out.println();
+    headerColorSort = String.format(HEADER, RESET, RESET, RESET, RESET, RESET, RESET, PURPLE, RESET);
+    System.out.println(headerColorSort);
+    SortStatusAndDatePlan sortRule = new SortStatusAndDatePlan();
+    Collections.sort(taskList, sortRule);
+    for (Task task : taskList) {
+      System.out.print(task);
     }
+    System.out.println(FOOTER);
+    Menu.menuSortChoice(br, taskList);
+  }
 
-    public static void sortByStatusAndName(BufferedReader br, List<Task> taskList) throws IOException {
-        System.out.println();
-        headerColorSort = String.format(HEADER, RESET, RESET, RESET, RESET, PURPLE, RESET, RESET, RESET);
-        System.out.println(headerColorSort);
-        SortDatePlanAndDescription sortRule = new SortDatePlanAndDescription();
-        Collections.sort(taskList, sortRule);
-        for (Task task : taskList) {
-            System.out.print(task);
-        }
-        System.out.println(FOOTER);
-        Menu.menuSortChoice(br, taskList);
+  public static void sortByStatusAndName(BufferedReader br, List<Task> taskList) throws IOException {
+    System.out.println();
+    headerColorSort = String.format(HEADER, RESET, RESET, RESET, RESET, PURPLE, RESET, RESET, RESET);
+    System.out.println(headerColorSort);
+    SortDatePlanAndDescription sortRule = new SortDatePlanAndDescription();
+    Collections.sort(taskList, sortRule);
+    for (Task task : taskList) {
+      System.out.print(task);
     }
+    System.out.println(FOOTER);
+    Menu.menuSortChoice(br, taskList);
+  }
 
-
-    public static void writeTaskToFile(List<Task> taskList) throws IOException {
-        File file = new File("res/TaskList.csv");
-        if (!file.exists()) {
-            System.out.print(RED + "... файл не найден ..." + RESET);
-            if (file.createNewFile()) {
-                System.out.print(YELLOW + "... файл создан ..." + RESET);
-            } else {
-                System.out.print(RED + "... файл не создан ..." + RESET);
-                return;
-            }
-        } else {
-            System.out.print(YELLOW + "... файл найден ..." + RESET);
-        }
-        FileWriter fileWriter = new FileWriter(file);
-        for (Task task : taskList) {
-            String isDone = task.getIsDone() ? "1" : "0";
-            String planeDateStr = DataUtils.getDateToStrToFile(task.getPlanDate());
-            String createDateStr = DataUtils.getDateToStrToFile(task.getCreateDate());
-            String description = task.getDescription().equals(" -не указано-") ? "" : task.getDescription();
-            String result =
-                    task.getTitle() + ";" + description + ";" + task.getCategory() + ";"
-                            + task.getPriority() + ";" + planeDateStr + ";" + isDone + ";" + createDateStr;
-            fileWriter.write(result + "\n");
-        }
-        fileWriter.close();
-        System.out.println(GREEN + "... файл сохранен!" + RESET);
-
+  public static void writeTaskToFile(List<Task> taskList) throws IOException {
+    File file = new File("res/TaskList.csv");
+    if (!file.exists()) {
+      System.out.print(RED + "... файл не найден ..." + RESET);
+      if (file.createNewFile()) {
+        System.out.print(YELLOW + "... файл создан ..." + RESET);
+      } else {
+        System.out.print(RED + "... файл не создан ..." + RESET);
+        return;
+      }
+    } else {
+      System.out.print(YELLOW + "... файл найден ..." + RESET);
     }
+    FileWriter fileWriter = new FileWriter(file);
+    for (Task task : taskList) {
+      String isDone = task.getIsDone() ? "1" : "0";
+      String planeDateStr = DataUtils.getDateToStrToFile(task.getPlanDate());
+      String createDateStr = DataUtils.getDateToStrToFile(task.getCreateDate());
+      String description = task.getDescription().equals(" -не указано-") ? "" : task.getDescription();
+      String result =
+          task.getTitle() + ";" + description + ";" + task.getCategory() + ";"
+          + task.getPriority() + ";" + planeDateStr + ";" + isDone + ";" + createDateStr;
+      fileWriter.write(result + "\n");
+    }
+    fileWriter.close();
+    System.out.println(GREEN + "... файл сохранен!" + RESET);
+
+  }
 
 }
--- src/Task.java
+++ src/Task.java
@@ -1,91 +1,108 @@
+import java.io.IOException;
 import java.util.Date;
-
+import java.util.List;
 
 public class Task {
-    private static int count = 0;
-    private int id; //уникальный идентификатор задачи
-    private String title;//название задачи
-    private String description; // описание задачи
-    private Category category; // enum категории
-    private Priority priority; // enum приоритет
-    private Date planDate; // дата, на которую запланирована задача
+  private static int count = 0;
+  private int id; //уникальный идентификатор задачи
+  private String title;//название задачи
+  private String description; // описание задачи
+  private Category category; // enum категории
+  private Priority priority; // enum приоритет
+  private Date planDate; // дата, на которую запланирована задача
 
-    private Date createDate;
-    private boolean isDone; // флаг выполнения
+  private Date createDate;
+  private boolean isDone; // флаг выполнения
 
-    public Task(String title, String description, Category category, Priority priority,
-                Date planDate, boolean isDone, Date createDate) {
-        this.id = ++count;
-        this.title = title;
-        this.description = description;
-        this.category = category;
-        this.priority = priority;
-        this.planDate = planDate;
-        this.isDone = isDone;
-        this.createDate = createDate;
+  public Task(String title, String description, Category category, Priority priority,
+              Date planDate, boolean isDone, Date createDate) {
+    this.id = ++count;
+    this.title = title;
+    this.description = description;
+    this.category = category;
+    this.priority = priority;
+    this.planDate = planDate;
+    this.isDone = isDone;
+    this.createDate = createDate;
+  }
+
+  @Override
+  public String toString() {
+    String id1 = "00" + id;
+    id1 = id1.substring(id1.length() - 2);
+    String title1 = title;
+    if (title1.length() > 24) {
+      title1 = title1.substring(0, 22) + "..";
     }
-
-    @Override
-    public String toString() {
-        String id1 = "00" + id;
-        id1 = id1.substring(id1.length() - 2);
-        String title1 = title;
-        if (title1.length() > 24) {
-            title1 = title1.substring(0, 22) + "..";
-        }
-        String description1 = description;
-        if (description1.isEmpty()) {
-            description1 = " -не указано-";
-        } else if (description1.length() > 39) {
-            description1 = description1.substring(0, 37) + "..";
-        }
-        String category1 = category.getName();
-        String priority1 = priority.getName();
-        String date1 = DataUtils.getDateToStr(planDate);
-        String isDone1 = isDone ? "✔" : "⏰";
-        return String.format("""
-                        │%3s │%-25s│%-40s│%10s │   %10s │%11s│%s  %s   │%n""",
-                id1, title1, description1, category1, priority1, date1, Menu.SPACE, isDone1);
+    String description1 = description;
+    if (description1.isEmpty()) {
+      description1 = " -не указано-";
+    } else if (description1.length() > 39) {
+      description1 = description1.substring(0, 37) + "..";
     }
+    String category1 = category.getName();
+    String priority1 = priority.getName();
+    String date1 = DataUtils.getDateToStr(planDate);
+    String isDone1 = isDone ? "✔" : "⏰";
+    return String.format("""
+            │%3s │%-25s│%-40s│%10s │   %10s │%11s│%s  %s   │%n""",
+        id1, title1, description1, category1, priority1, date1, Menu.SPACE, isDone1);
+  }
 
+  public int getId() {
+    return id;
+  }
 
-    public int getId() {
-        return id;
+  public String getTitle() {
+    return title;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public Category getCategory() {
+    return category;
+  }
+
+  public Priority getPriority() {
+    return priority;
+  }
+
+  public Date getPlanDate() {
+    return planDate;
+  }
+
+  public boolean getIsDone() {
+    return isDone;
+  }
+
+  public static void taskDeleteById(List<Task> taskList) throws IOException {
+    System.out.print("Введите Id задачи которую хотите удалить: ");
+    int id = Input.readIntLimited(0, count);
+    boolean isNotFound = true;
+    for (int i = 0; i < taskList.size(); i++) {
+      if (taskList.get(i).getId() == id) {
+        taskList.remove(i);
+        System.out.println(Menu.CYAN + "Задача с Id:" + id + " удалена!" + Menu.RESET);
+        isNotFound = false;
+        break;
+      }
     }
-
-    public String getTitle() {
-        return title;
+    if (isNotFound) {
+      System.out.println(Menu.RED + "Задача с Id:" + id + " не найдена!" + Menu.RESET);
     }
+  }
 
-    public String getDescription() {
-        return description;
-    }
+  public void markAsDone() {
+    this.isDone = true;
+  }
 
-    public Category getCategory() {
-        return category;
-    }
+  public void markAsUndone() {
+    this.isDone = false;
+  }
 
-    public Priority getPriority() {
-        return priority;
-    }
-
-    public Date getPlanDate() {
-        return planDate;
-    }
-
-    public boolean getIsDone() {
-        return isDone;
-    }
-
-    public void markAsDone() {
-        this.isDone = true;
-    }
-
-    public void markAsUndone() {
-        this.isDone = false;
-    }
-
-    public Date getCreateDate() {
-        return createDate;
-    }
+  public Date getCreateDate() {
+    return createDate;
+  }
 }
