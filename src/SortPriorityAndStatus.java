8ade27760b7bf133c6fb0763da9e13408159990b
Vadim Balanciuc <b.vadik2017@gmail.com>
2023-03-30 22:15:32 +0200

    refactor SortPriorityAndStatus

--- src/SortPriorityAndStatus.java
+++ src/SortPriorityAndStatus.java
@@ -4,7 +4,7 @@ public class SortPriorityAndStatus implements Comparator<Task> {
   @Override
   public int compare(Task o1, Task o2) {
     if (o1.getPriority() == o2.getPriority()) {
-      Boolean.compare(o1.getIsDone(), o2.getIsDone());
+      return Boolean.compare(o1.getIsDone(), o2.getIsDone());
     }
     return o1.getPriority().compareTo(o2.getPriority());
   }
