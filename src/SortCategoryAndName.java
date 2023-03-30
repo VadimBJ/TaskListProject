6674fe66f4faf49bc9a492ac3e03057753f92c24
Yana Tykhonenko <tihonenkoyana@gmail.com>
2023-03-30 21:45:16 +0200

    Add SortCategoryAndName()

---
+++ src/SortCategoryAndName.java
@@ -0,0 +1,13 @@
+import java.util.Comparator;
+
+public class SortCategoryAndName implements Comparator<Task> {
+  @Override
+  public int compare(Task o1, Task o2) {
+    if (o1.getCategory().getName().compareTo(o2.getCategory().getName())==0) {
+      o1.getTitle().compareTo(o2.getTitle());
+    }
+    return o1.getCategory().getName().compareTo(o2.getCategory().getName());
+  }
+
+}
+
