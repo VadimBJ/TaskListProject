44f55e57686338b063ad086fcf67d6aef8c3f421
Yana Tykhonenko <tihonenkoyana@gmail.com>
2023-03-30 21:45:54 +0200

    Add SortPriorityAndStatus()

---
+++ src/SortPriorityAndStatus.java
@@ -0,0 +1,11 @@
+import java.util.Comparator;
+
+public class SortPriorityAndStatus implements Comparator<Task> {
+  @Override
+  public int compare(Task o1, Task o2) {
+    if (o1.getPriority() == o2.getPriority()) {
+      Boolean.compare(o1.getIsDone(), o2.getIsDone());
+    }
+    return o1.getPriority().compareTo(o2.getPriority());
+  }
+}
