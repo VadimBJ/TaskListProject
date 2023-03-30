d3f94041a81f1c44d92cf2a359f24ede23295512
Vadim Balanciuc <b.vadik2017@gmail.com>
2023-03-30 22:15:45 +0200

    refactor SortStatusAndDatePlan

--- src/SortStatusAndDatePlan.java
+++ src/SortStatusAndDatePlan.java
@@ -1,28 +1,25 @@
 import java.util.Comparator;
 
-
 public class SortStatusAndDatePlan implements Comparator<Task> {
 
-//по статусу/дата запланированная/дата создания
-
-    @Override
-    public int compare(Task o1, Task o2) {
-        if (Boolean.compare(o1.getIsDone(), o2.getIsDone()) == 0) {
-            if (o1.getPlanDate() == null && o2.getPlanDate() == null) {
-                return o1.getCreateDate().compareTo(o2.getCreateDate());
-            }
-            if (o1.getPlanDate() == null && o2.getPlanDate() != null) {
-                return -1;
-            }
-            if (o1.getPlanDate() != null && o2.getPlanDate() == null) {
-                return 1;
-            }
-            if (o1.getPlanDate().compareTo(o2.getPlanDate()) == 0) {
-                return o1.getCreateDate().compareTo(o2.getCreateDate());
-            }
-            return o1.getPlanDate().compareTo(o2.getPlanDate());
-        }
-        return Boolean.compare(o1.getIsDone(), o2.getIsDone());
-
+  @Override
+  public int compare(Task o1, Task o2) {
+    if (Boolean.compare(o1.getIsDone(), o2.getIsDone()) == 0) {
+      if (o1.getPlanDate() == null && o2.getPlanDate() == null) {
+        return o1.getCreateDate().compareTo(o2.getCreateDate());
+      }
+      if (o1.getPlanDate() == null && o2.getPlanDate() != null) {
+        return -1;
+      }
+      if (o1.getPlanDate() != null && o2.getPlanDate() == null) {
+        return 1;
+      }
+      if (o1.getPlanDate().compareTo(o2.getPlanDate()) == 0) {
+        return o1.getCreateDate().compareTo(o2.getCreateDate());
+      }
+      return o1.getPlanDate().compareTo(o2.getPlanDate());
     }
+    return Boolean.compare(o1.getIsDone(), o2.getIsDone());
+
+  }
 }
